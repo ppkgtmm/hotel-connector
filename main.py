20978b3465a1576f2204aeaa196eda07144ac7c2
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-06 01:12:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from setup_user.main import setup_replication_user
+from register_source.main import register_source_database
+
 
 def entrypoint():
     setup_replication_user()
+    register_source_database()
