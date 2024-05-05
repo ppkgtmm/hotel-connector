9be4587a1b89cf96a9ac5fef5c146ed95fd8c3da
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-06 00:58:20 +0700

    Create main.py

---
+++ register_source/main.py
@@ -0,0 +1,28 @@
+from os import getenv
+import requests
+import json
+
+def get_configuration ():
+    config_path = f"register_source/config.json"
+    with open(config_path, "r") as fp:
+        config = fp.read()
+    config = (
+        config.replace("${DB_HOST}", getenv("DB_HOST").split(':')[0])
+        .replace("${DBZ_USER}", getenv("DBZ_USER"))
+        .replace("${DBZ_PASSWORD}", getenv("DBZ_PASSWORD"))
+        .replace("${DB_NAME}", getenv("DB_NAME"))
+    )
+    return config
+
+def register_source_database():
+    config = get_configuration()
+    endpoint = f"{getenv("KAFKA_CONNECT_SERVER")}/connectors/"
+    response = requests.post(
+        endpoint,
+        headers={
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        json=json.loads(config)
+    )
+    assert response.status_code == 201
