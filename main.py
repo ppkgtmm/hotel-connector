d86849936ce47e03accffe212953c7e3369f25a9
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-08 00:04:28 +0700

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from setup_user.main import setup_replication_user
-from register_source.main import register_source_database
-
-
-def entrypoint():
-    setup_replication_user()
-    register_source_database()
