6250937305f1ead66b806d93bd0c799d9a2eb068
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-10 23:40:58 +0700

    Update queries.sql

--- lambda/queries.sql
+++ lambda/queries.sql
@@ -7,5 +7,3 @@ GRANT rds_superuser TO ${DBZ_USER};
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO ${DBZ_USER};
 
 GRANT CREATE ON DATABASE ${DB_NAME} TO ${DBZ_USER};
-
-SELECT 'ALTER TABLE "' || table_name || '" OWNER TO ${DBZ_USER};' from information_schema.tables where table_schema = 'public' \gexec
