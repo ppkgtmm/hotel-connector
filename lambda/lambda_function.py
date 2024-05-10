4a939a0f31756e582c2115aa257dc1059d76da18
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-10 19:11:34 +0700

    Revert "Update lambda_function.py"
    This reverts commit eaeae11049f6f64936f80717c94e64989c807bd5.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,44 +1,45 @@
+from os import getenv
 import requests
 import json
 from sqlalchemy import create_engine, text
 
 
-def get_configuration(DB_HOST: str, DBZ_USER: str, DBZ_PASSWORD: str, DB_NAME: str):
+def get_configuration():
     with open("connector-config.json", "r") as fp:
         config = fp.read()
     config = (
-        config.replace("${DB_HOST}", DB_HOST.split(":")[0])
-        .replace("${DBZ_USER}", DBZ_USER)
-        .replace("${DBZ_PASSWORD}", DBZ_PASSWORD)
-        .replace("${DB_NAME}", DB_NAME)
+        config.replace("${DB_HOST}", getenv("DB_HOST").split(":")[0])
+        .replace("${DBZ_USER}", getenv("DBZ_USER"))
+        .replace("${DBZ_PASSWORD}", getenv("DBZ_PASSWORD"))
+        .replace("${DB_NAME}", getenv("DB_NAME"))
     )
     return config
 
 
-def register_source_database(config: str, KAFKA_CONNECT_SERVER:str):
-    endpoint = f"{KAFKA_CONNECT_SERVER}/connectors/"
+def register_source_database(config: str):
+    endpoint = f"{getenv("KAFKA_CONNECT_SERVER")}/connectors/"
     headers = {"Accept": "application/json", "Content-Type": "application/json"}
     response = requests.post(endpoint, headers=headers, json=json.loads(config))
     return "success" if response.status_code == 201 else "failed"
 
 
-def get_query_template(DBZ_USER: str, DBZ_PASSWORD: str, DB_NAME: str):
+def get_query_template():
     with open("queries.sql", "r") as fp:
         template = fp.read()
     template = (
-        template.replace("${DBZ_USER}", DBZ_USER)
-        .replace("${DBZ_PASSWORD}", DBZ_PASSWORD)
-        .replace("${DB_NAME}", DB_NAME)
+        template.replace("${DBZ_USER}", getenv("DBZ_USER"))
+        .replace("${DBZ_PASSWORD}", getenv("DBZ_PASSWORD"))
+        .replace("${DB_NAME}", getenv("DB_NAME"))
     )
     return template
 
 
 def lambda_handler(event, context):
-    engine = create_engine(f'postgresql://{event['DB_USER']}:{event['DB_PASSWORD']}@{event['DB_HOST']}/{event['DB_NAME']}')
+    engine = create_engine(f'postgresql://{getenv("DB_USER")}:{getenv("DB_PASSWORD")}@{getenv("DB_HOST")}/{getenv("DB_NAME")}')
     conn = engine.connect()
-    template = get_query_template(event['DBZ_USER'], event['DBZ_PASSWORD'], event['DB_NAME'])
+    template = get_query_template()
     conn.execute(text(template))
     conn.close()
     engine.dispose()
-    config = get_configuration(event['DB_HOST'], event['DBZ_USER'], event['DBZ_PASSWORD'], event['DB_NAME'])
-    return register_source_database(config, event['KAFKA_CONNECT_SERVER'])
+    config = get_configuration()
+    return register_source_database(config)
