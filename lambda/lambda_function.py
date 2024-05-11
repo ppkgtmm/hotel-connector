2265df3b860643426ac824785e9e50e01c43781a
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-11 19:43:36 +0700

    Update lambda_function.py

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -12,7 +12,6 @@ def get_configuration():
         .replace("${DBZ_USER}", getenv("DBZ_USER"))
         .replace("${DBZ_PASSWORD}", getenv("DBZ_PASSWORD"))
         .replace("${DB_NAME}", getenv("DB_NAME"))
-        .replace("${KAFKA_BOOTSTRAP_SERVER}", getenv("KAFKA_BOOTSTRAP_SERVER"))
     )
     return config
 
