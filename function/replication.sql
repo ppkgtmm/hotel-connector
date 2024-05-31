b5363e32a66b3545a9851dcfe2a360f60f63818d
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-31 20:50:52 +0700

    remove old files

--- function/replication.sql
+++
@@ -1,9 +0,0 @@
-CREATE USER ${DBZ_USER} PASSWORD '${DBZ_PASSWORD}';
-
-GRANT rds_superuser, rds_replication TO ${DBZ_USER};
-
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO ${DBZ_USER};
-
-GRANT CREATE ON DATABASE ${DB_NAME} TO ${DBZ_USER};
-
-SELECT table_name from information_schema.tables where table_schema = 'public';
