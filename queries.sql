4c6ca31966b6cea9ca5fafdebe27071e8fac6509
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-31 22:43:23 +0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -2,4 +2,4 @@ ALTER USER ${DB_USER} REPLICATION;
 
 GRANT CREATE ON DATABASE ${DB_NAME} TO ${DB_USER};
 
-SELECT table_name from information_schema.tables where table_schema = 'public';
+select tablename from pg_tables where schemaname = 'public';
