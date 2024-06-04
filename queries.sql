5600b675640fbbed7af61b489dc92b2e79ae52fa
Pinky Gautam <pkgtm1998@gmail.com>
2024-06-04 23:39:18 +0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -2,4 +2,4 @@ ALTER USER ${DB_USER} REPLICATION;
 
 GRANT CREATE ON DATABASE ${DB_NAME} TO ${DB_USER};
 
-SELECT tablename FROM pg_tables WHERE schemaname = 'public' AND tablename NOT IN ('user') ;
+SELECT tablename FROM pg_tables WHERE schemaname = 'public' AND tablename NOT IN ('user');
